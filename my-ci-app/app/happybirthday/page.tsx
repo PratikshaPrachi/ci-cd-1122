7792678687bec5c6e2a99b137d204ebbf7c49c2c
Partha S Jena <pratikshaprachi99@gmail.com>
2025-06-24 10:08:41 +0530

    change in deploy.yml 2

--- my-ci-app/app/happybirthday/page.tsx
+++ my-ci-app/app/happybirthday/page.tsx
@@ -16,10 +16,10 @@ export default function HappyBirthday() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-pink-100 via-red-200 to-yellow-100 p-6 text-center">
       <h1 className="text-4xl md:text-6xl font-bold text-pink-700 mb-4">
-        🎉 Happy Birthday Harkirat! 🎂
+        🎉 Happy Birthday Parth! 🎂
       </h1>
       <p className="text-xl md:text-2xl text-gray-700 max-w-xl">
-        Wishing you a day full of joy, laughter, and lots of cake! Your impact on the dev community is truly appreciated.
+        Wishing you a day full of joy, laughter, and lots of cake! .
       </p>
 
       <Image
